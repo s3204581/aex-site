da53bc2049e05b20fd7debfaa4d6900b626726f8
s3376686 <s3376686@student.rmit.edu.au>
2023-08-17 17:46:22 +1000

    removed unused components

--- frontend/src/components/ColourText.jsx
+++
@@ -1,7 +0,0 @@
-import { Box } from "@mui/material";
-
-function ColourText({ color, children }) {
-    return (<Box sx={{color}} component='span'>{children}</Box>);
-}
-
-export default ColourText;
--- frontend/src/router/AppRoutes.jsx
+++ frontend/src/router/AppRoutes.jsx
@@ -1,6 +1,4 @@
 import Dashboard from '../containers/Dashboard/Dashboard';
-import FileDropzone from '../containers/Scan/Scan';
-import Analytics from '../containers/Analytics/Analytics';
 import EmailScanDropzone from '../containers/Scan/Scan';
 import ThreatIndicatorsPage from '../containers/Threats/Threats';
 import SettingsPage from '../containers/Settings/Settings';
@@ -11,7 +9,6 @@ function AppRoutes() {
         <Route path='/' element={<Dashboard/>} />
         <Route path='/dashboard' element={<Dashboard/>} />
         <Route path='/scan' element={<EmailScanDropzone/>} />
-        <Route path='/analytics' element={<Analytics/>} />
         <Route path='/threats' element={<ThreatIndicatorsPage/>} />
         <Route path='/settings' element={<SettingsPage/>} />
     </Routes>
